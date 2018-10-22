dc062dd9c6a07b7efc7e2be74d442cecda90b577
Brian Simpson <telnettech@gmail.com>
2018-10-21 22:42:33 -0400

    3rd draft of Final Project

--- README.md
+++ README.md
@@ -3,38 +3,36 @@
 10/21/18 (Brian Simpson)
   --Added Nav buttons and styles with Columbus Crew colors
   --Added Social Media and links to urls
+  --Added "Social Media" button in Nav Bar
+  --Added "Contact Me" button in Nav Bar
+  --Added Javascript to display current time and date above Nav Bar
   --Changed color of #AboutMe section to CC Yellow
   --Resolved <p> element wrapping
-  
+  --Resolved background color being too long
+
   Current Issues:
-    --List bullets on socialMedia links.
 	--CSS for each socialMedia link. Got to be a better way.
-    --"Contact Me" button not opening mailto link when href 'mailto:telnettech@gmail.com' is added.
-    --"AboutMe" text box has too much space at bottom.
-    
+	--Javascript to change backgroundImage not working.
+
   To Do List:
-    --Create pages 2 thru 4 per wireframe or Pop up Box for Nav Buttons?
-    --Hamburger menu for pages
-    --Form for page 4
-    --Javascript stuff
+    --Create pages 2 thru 4 per wireframe or Modal Box for Nav Buttons?
+    --Hamburger menu for Nav Bar
+    --Form for "Suggestion" button
     --Media queries for 'Mobile First' approach
-  
+
 
 10/14/18 (Brian Simpson)
   --Rough draft start to 1st page of project
   --created index.html and css with flexbox elements. added images to project folder.
-  
+
   Current Issues:
     --<p> element not wrapping in 1st column
     --missing hamburger menu and elements for page 1.
     --pages 2 thru 4 are missing.
-  
+
   To Do List:
     --Create pages 2 thru 4 per wireframe
     --Hamburger menu for pages
     --Form for page 4
     --Javascript stuff
     --Media queries for 'Mobile First' approach
-    
-    
-  
--- js/app.js
+++ js/app.js
@@ -1,30 +1,61 @@
-/* Code for Hamburger Menu */
+// START CLOCK SCRIPT
 
-var wrapperMenu = document.querySelector('.wrapper-menu');
+function startTime() {
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+    hr = (hr == 0) ? 12 : hr;
+    hr = (hr > 12) ? hr - 12 : hr;
+    //Add a zero in front of numbers<10
+    hr = checkTime(hr);
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
 
-wrapperMenu.addEventListener('click', function(){
-  wrapperMenu.classList.toggle('open');
-})
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+    document.getElementById("date").innerHTML = date;
 
-
-/*Code for Atomic Clock callback*/
-
-function getTime(zone, success) {
-    var url = 'http://json-time.appspot.com/time.json?tz=' + zone,
-        ud = 'json' + (+new Date());
-    window[ud]= function(o){
-        success && success(new Date(o.datetime));
-    };
-    document.getElementsByTagName('head')[0].appendChild((function(){
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.src = url + '&callback=' + ud;
-        return s;
-    })());
+    var time = setTimeout(function(){ startTime() }, 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
 
-getTime('GMT', function(time){
-    // This is where you do whatever you want with the time:
-    alert(time);
-});
+// END CLOCK SCRIPT
 
+// START BACKGROUND IMAGE
+
+// var images=['../img/thomas-jensen-596085-unsplash.png',
+//             '../img/markus-spiske-207946-unsplash.png',
+//             '../img/markus-spiske-357131-unsplash.png',
+//             '../img/110-PunchDown-Block.jpg'
+//           ];
+//
+// for (var = i; i<images.length; i++){
+//   	body.style.backgroundImage(url(images[i]));
+// }
+
+
+// var url=0;
+//
+// setInterval(function(){
+//    url+=1;
+//   if(url==4){
+//     url=0;
+//   }
+//   document.body.style.backgroundImage = 'url('+images[url]+')';
+//   // document.body.style.backgroundRepeat = "no-repeat";
+// },3000);
+
+// END BACKGROUND IMAGE
