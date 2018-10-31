4502ba2279c6fbd7aee4c4323e037a22cb236d6e
Brian Simpson <telnettech@gmail.com>
2018-10-31 18:09:04 -0400

    Modal Box added but broken

--- js/app.js
+++ js/app.js
@@ -72,3 +72,31 @@ $(document).ready(function(){
 });
 
 // END HAMBURGER MENU
+
+// START MODAL BOX FOR "MY RESUME" BUTTON
+// used this reference: https://www.w3schools.com/howto/howto_css_modals.asp
+// and modified per MDN for the Event Listener
+
+var modal = document.getElementById('myResume');
+var btn = document.getElementById('myBtn');
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.addEventListener('click',function() {
+    console.log('clicked');
+    modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.addEventListener('click',function() {
+    modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click',function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}); 
+
+//END MODAL BOX FOR "MY RESUME" BUTTON
